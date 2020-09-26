85160f07e1e66121c565e482a7891b934fcc9e21
Ramachandran SS <ramachandranss22@gmail.com>
2020-09-26 11:17:08 +0000

    HomeService Robot V1.0

--- HomeService/build/add_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/HomeService/src/add_markers
-# Build directory: /home/workspace/HomeService/build/add_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- HomeService/build/pick_objects/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/HomeService/src/pick_objects
-# Build directory: /home/workspace/HomeService/build/pick_objects
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
